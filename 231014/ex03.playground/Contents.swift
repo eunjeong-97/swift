d6a6825515ab852c5deb2fa363858a7f181c032b
박은정 <eoss7777@naver.com>
2023-10-14 15:10:52 +0900

    숫자형태의 데이터타입
    - 정수타입 Int, UInt
    - 실수타입: Float, Double
    -

---
+++ 231014/ex03.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+/*
+ [변수의 타입] 
+ 
+ [데이터 타입]
+ 데이터를 저장하는 메모리 공간의, 크기와 내부 구조 모양
+ 
+ [자료형]
+ literal: data 값 자체
+ number literal: 숫자 데이터
+ 
+ 정수 Integer Type
+ 실수 Float Type
+ 
+ */
+
+
+/* 정수타입
+ Int8
+ Int16
+ Int32
+ Int64
+
+U: Unsign된 Int데이터
+  UInt8
+  UInt16
+  UInt32
+  UInt64
+ 
+ */
+print(Int8.max)
+print(Int16.max)
+print(Int32.max)
+print(Int64.max)
+
+/*
+ 실수타입
+ floating point 소수점
+ Float Type: 32비트
+ Double Tpe: 64비트
+ */
