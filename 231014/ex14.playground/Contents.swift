2b306f4b9a5500ba6fffd69981bf52c18ab6f9f2
eunjeong-97 <eoss7777@naver.com>
2023-10-20 12:56:52 +0900

    조건문 연습 #2

--- 231014/ex14.playground/Contents.swift
+++ 231014/ex14.playground/Contents.swift
@@ -1,18 +1,34 @@
 import UIKit
 
-let score = 90
+let scoreList: [Int] = [90, 80, 70, 60]
+let creditList: [String] = ["A", "B", "C", "D"]
 
-let scoreList: [Int] = [90, 80, 70, 60, 50]
-let creditList: [String] = ["A", "B", "C", "D", "F"]
-
+// 점수에 따라 학점 리턴
 func checkScore(num:Int) -> String {
     if num < 0 || num > 100 {
         return "올바른 점수를 입력해주세요"
     }
+    var credit = "F"
     for (index, baseline) in scoreList.enumerated() {
-        print(index, " ", baseline)
+        if(num>=baseline) {
+            credit = creditList[index]
+            break
+        }
     }
-    return ""
+    return credit;
 }
 
-checkScore(num: score)
+var score = 90
+print(score, checkScore(num: score))
+
+score = 84
+print(score, checkScore(num: score))
+
+score = 75
+print(score, checkScore(num: score))
+
+score = 69
+print(score, checkScore(num: score))
+
+score = 50
+print(score, checkScore(num: score))
