fab4766f8361e1935ed14912bf7cbbd6092c8d4d
eunjeong-97 <eoss7777@naver.com>
2023-10-21 15:37:56 +0900

    for문 연습#2 구구단

--- 231021/ex06.playground/Contents.swift
+++ 231021/ex06.playground/Contents.swift
@@ -1,1 +1,26 @@
+import UIKit
+
+/*
+ 중첩 for문
+ nested for statement
+ 
+ 구구단
+ 단수 X 곱하는 수
+ 1      1~9
+ 2      1~9
+ 3      1~9
+ 4      1~9
+ 5      1~9
+ 6      1~9
+ 7      1~9
+ 8      1~9
+ 9      1~9
+ */
+
+for first in 1...9 {
     print("====== \(first)단 =====")
+    for second in 1...9 {
+        print("\(first) X \(second) = \(first * second)")
+    }
+    print("")
+}
