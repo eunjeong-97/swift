a6079e9696617cafa3733747617e65a81cc15889
eunjeong-97 <eoss7777@naver.com>
2023-11-04 17:37:32 +0900

    wip: 화면이동

---
+++ 231104/App02/App02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App02
+//
+//  Created by 더조은-3 on 2023/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
